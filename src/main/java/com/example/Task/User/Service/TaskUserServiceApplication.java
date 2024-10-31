595a9733cab932d3f572e28fecdc8ed057f7b145
Rohit kumar <rohitkumarrr2022@gmail.com>
2024-10-31 17:15:22 +0530

    Today I have added the configuration class sets up Spring Security for my application , defiing how authentication and authorizaton work and it also defines stateless session management, CORS settings, and custom request handling via JWT Tokens and I have also added a secure way to handle user passwords Inshort:- The classes which present inside the config package that classes is essential for protecting my application and ensuring that only authenticated users can access certain endpoints while also handling CORs and securing user credentails

--- src/main/java/com/example/Task/User/Service/TaskUserServiceApplication.java
+++ src/main/java/com/example/Task/User/Service/TaskUserServiceApplication.java
@@ -8,6 +8,7 @@ public class TaskUserServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskUserServiceApplication.class, args);
+		System.out.println("hello world");
 	}
 
 }
