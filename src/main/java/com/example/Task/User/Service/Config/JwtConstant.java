df3c488cc28901d1686bbb3291aff87ce309b46a
Rohit kumar <rohitkumarrr2022@gmail.com>
2024-11-01 20:18:57 +0530

    Created new class JwtConstant class for the secret key and header and added authoority inside the JwtTokenValidator class

---
+++ src/main/java/com/example/Task/User/Service/Config/JwtConstant.java
@@ -0,0 +1,6 @@
+package com.example.Task.User.Service.Config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY="alsdfjkweoriuzxcmnvalsdjf230asdlflkjasdflk04u278lfj";
+    public static final String JWT_HEADER="Authorization";
+}
--- src/main/java/com/example/Task/User/Service/Config/JwtTokenValidator.java
+++ src/main/java/com/example/Task/User/Service/Config/JwtTokenValidator.java
@@ -1,13 +1,21 @@
 package com.example.Task.User.Service.Config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import javax.crypto.SecretKey;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 public class JwtTokenValidator extends OncePerRequestFilter {
     @Override
@@ -17,6 +25,23 @@ public class JwtTokenValidator extends OncePerRequestFilter {
                                     FilterChain filterChain
     ) throws ServletException, IOException
     {
-//        String jwt = request.getHeader();
+        String jwt = request.getHeader(JwtConstant.JWT_HEADER);
+        if(jwt != null)
+        {
+            jwt = jwt.substring(7);
+            try
+            {
+                SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+                Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+                String emails = String.valueOf(claims.get("email"));
+                String authorities = String.valueOf(claims.get("authorities"));
+
+                List<GrantedAuthority> auth = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
     }
 }
